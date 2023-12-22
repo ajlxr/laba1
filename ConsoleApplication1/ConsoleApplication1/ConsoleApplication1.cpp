1fe875e7ec732057cec641406d2a4b8aa71961aa
Alex <121189527+ajlxr@users.noreply.github.com>
2023-12-22 17:22:03 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+  
+    int ChisloSlov;
+
+    cout << "Введите число слов: ";
+    cin >> ChisloSlov;
+
+    string ImyaFaila;
+
+    ifstream file("C:/Users/kiril/Desktop/laba7.txt");
+
+    string sentence;
+
+    while (getline(file, sentence)) {
+ 
+        vector<string> words;
+
+        istringstream iss(sentence);
+        do {
+            string word;
+            iss >> word;
+
+            if (!word.empty()) {
+                words.push_back(word);
+            }
+        } while (iss);
+
+        
+        if (words.size() == ChisloSlov) {
+            cout << sentence << endl;
+        }
+    }
+
+    file.close();
+
+    return 0;
+}
